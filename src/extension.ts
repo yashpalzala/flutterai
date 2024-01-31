36c8239ce77e2b8d4d6e9c907a6250e488476280
yash <yzala999@gmail.com>
2024-01-31 21:27:37 +0530

    [CHORE] converted JS to TS

---
+++ src/extension.ts
@@ -0,0 +1,163 @@
+import * as vscode from 'vscode';
+import fetch from 'node-fetch';
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context: vscode.ExtensionContext) {
+    let disposableGenCode = vscode.commands.registerCommand("flutterai.generateDartCode", async () => {
+        await mainFunc(context);
+    });
+    context.subscriptions.push(disposableGenCode);
+
+    // Enter/Replace key command
+    let enterKeyCommand = vscode.commands.registerCommand("flutterai.enterOnlyKey", async () => {
+        await setApiKey(context);
+    });
+    context.subscriptions.push(enterKeyCommand);
+}
+
+async function mainFunc(context: vscode.ExtensionContext) {
+    // get and set the key first
+    let apiKey = await getApiKey(context);
+    if (!apiKey) {
+        apiKey = await setApiKey(context);
+    }
+
+    if (!apiKey) {
+        return vscode.window.showErrorMessage('Terminated: API Key not found');
+    }
+
+    // Get the active text editor.
+    const editor = vscode.window.activeTextEditor!;
+
+    // Get the current contents of the active text editor.
+    const text = editor.document.getText();
+    const textAppend = 'only code and corresponding comments only if necessary. Don\'t write language name in the beginning and remove semi-colon or any ending symbol if present from last as it is a code snippet';
+    const userInputPrompt = await vscode.window.showInputBox({
+        prompt: 'Write a container with rounded corners with radius 8 in flutter',
+        value: 'Write a code snippet in flutter ...', // Optional: default value in the input box
+    });
+
+    // Prepare the API request body.
+    if (!userInputPrompt) return;
+    const body = {
+        contents: [
+            {
+                parts: [
+                    {
+                        "text": userInputPrompt + textAppend,
+                    },
+                ],
+            },
+        ],
+    };
+
+    // Prepare the API request headers.
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    let isCancelled = false;
+    let response;
+
+    await vscode.window.withProgress(
+        {
+            location: vscode.ProgressLocation.Notification,
+            title: 'Generating code...',
+            cancellable: true,
+        },
+        async (progress, token) => {
+            token.onCancellationRequested(() => {
+                // Handle cancellation (if needed)
+                isCancelled = true;
+                vscode.window.showInformationMessage('Task cancelled');
+            });
+
+            response = await fetch(
+                'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' +
+                apiKey,
+                {
+                    method: 'POST',
+                    headers,
+                    body: JSON.stringify(body),
+                }
+            );
+
+            // Optional: Report progress (if needed)
+            //   progress.report({ increment: 100 });
+
+            // Perform your long-running task here
+        }
+    );
+
+    // Get the API response body.
+    const data = await response!.json() ;
+    if (response!.status != 200) {
+        vscode.window.showErrorMessage('Something went wrong. REASON: ' + data['error']['message']);
+        return;
+    }
+
+    const resultText = data.candidates[0].content.parts[0].text;
+    if (!resultText) {
+        vscode.window.showErrorMessage('Something went wrong. REASON: ' + data.candidates[0].finishReason);
+        return;
+    }
+
+    // Get the generated code from the API response body.
+    const generatedCode = resultText;
+
+    // Validate the generated code.
+    //   const isValidDartCode = validateDartCode(generatedCode);
+
+    // If the generated code is valid, convert it to a Dart code snippet.
+    //   const dartCodeSnippet = isValidDartCode ? toDartCodeSnippet(generatedCode) : '';
+    const dartCodeSnippet = toDartCodeSnippet(generatedCode);
+
+    // Insert the generated code snippet into the active text editor.
+    if (editor && editor.selection && !isCancelled) {
+        // Insert the generated code snippet into the active text editor.
+        console.log(dartCodeSnippet);
+        editor.edit((editBuilder) => {
+            editBuilder.insert(editor.selection.start, dartCodeSnippet);
+        });
+    } else {
+        vscode.window.showErrorMessage('No active text editor or selection found.');
+    }
+}
+
+// Function to set API key
+async function setApiKey(context: vscode.ExtensionContext) {
+    const apiKey = await vscode.window.showInputBox({
+        prompt: 'API Key from here [link](https://makersuite.google.com/app/apikey)',
+        placeHolder: 'API Key',
+        password: true, // Hide the input (optional)
+        ignoreFocusOut: true,
+    });
+
+    if (apiKey) {
+        await context.secrets.store('flutterai.key', apiKey);
+        vscode.window.showInformationMessage('API key saved successfully!');
+        return apiKey;
+    }
+}
+
+// Function to get API key from secrets
+async function getApiKey(context: vscode.ExtensionContext) {
+    const apiKey = await context.secrets.get('flutterai.key');
+    return apiKey || undefined;
+}
+
+// Function to convert the generated code to a Dart code snippet.
+function toDartCodeSnippet(code: string) {
+    // Wrap the generated code in a Dart code snippet.
+    return code.replace(/```/g, '');
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+export {
+    activate,
+    deactivate
+};
